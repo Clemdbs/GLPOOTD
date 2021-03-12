ff0b2525dde21e3ec2b49adf1947b3b165c1871e
Clément <clement_dubois.etu@univ-lemans.fr>
2021-03-12 16:48:54 +0100

    vide

--- main.py
+++
@@ -1,5 +0,0 @@
-from membre import *
-
-#test aurélien
-if __name__ == '__main__':
-    Utilisateur1 = Utilisateur()
--- user.py
+++
@@ -1,8 +0,0 @@
-class Utilisateur:
-    def __init__(self):
-        self.nom = "Nom"
-        self.prenom = "Prénom"
-        self.email = "test@gmail.com"
-        self.numero_de_telephone = "06.00.00.00.00"
-        self.mot_de_passe = "azerty"
-        self.type = "Client"
